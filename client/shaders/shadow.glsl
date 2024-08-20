98f88832657e766a6563483b862fe1ae43d3f1b1
holyxtech <131367433+holyxtech@users.noreply.github.com>
2024-08-20 14:34:00 +1000

    blend shadow map cascades at border

--- client/shaders/shadow.glsl
+++ client/shaders/shadow.glsl
@@ -10,62 +10,70 @@ layout (std140, binding = 2) uniform ShadowBlock {
     uniform vec4 cascadePlaneDistances[(numCascades + 3) / 4];
 };
 
-const float shadowMagnitude = 0.7;
+const float shadowMagnitude = .7f;
+const float bias = 0.0005;
+float ShadowPCF(vec2 uvs, int cascadeIdx, float lsDepth) {
+    float shadow = 0.0;
+    float shadowDepth = texture(shadowMap, vec3(uvs, cascadeIdx)).r;
+    vec2 texelSize = 1.0 / vec2(textureSize(shadowMap, 0));
+    for (int x = -1; x <= 1; ++x) {
+        for (int y = -1; y <= 1; ++y) {
+            float pcfDepth = texture(shadowMap, vec3(uvs + vec2(x, y) * texelSize, cascadeIdx)).r;
+            shadow += (lsDepth - bias) > pcfDepth ? shadowMagnitude : 0.0;
+        }    
+    }
+    shadow /= 9.0;
+    return shadow;
+}
 
 float ShadowCalculation(vec3 worldPos, vec3 worldNormal, vec3 cameraPos) {
     if (dot(worldNormal, lightDir) <= 0)
-        return shadowMagnitude;
+        return shadowMagnitude; 
 
-    // select cascade layer
-    float depthValue = abs(cameraPos.z);
-
+    float vsDepth = abs(cameraPos.z);
+    float vsNextPlaneDistance;
     int layer = -1;
     for (int i = 0; i < numCascades; ++i) {
-        if (depthValue < cascadePlaneDistances[i/4][i%4]) {
+        vsNextPlaneDistance = cascadePlaneDistances[i/4][i%4];
+        if (vsDepth < vsNextPlaneDistance) {
             layer = i;
             break;
         }
     }
     if (layer == -1) 
-        layer = numCascades;
-
-    vec4 fragPosLightSpace = lightSpaceMatrices[layer] * vec4(worldPos, 1.0);
-    // perform perspective divide
-    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-    // transform to [0,1] range
-    projCoords = projCoords * 0.5 + 0.5;
-
-    // get depth of current fragment from light's perspective
-    float currentDepth = projCoords.z;
-
-    // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
-    if (currentDepth > 1.0)
         return 0.0;
 
-    // calculate bias (based on depth map resolution and slope)
-    vec3 normal = normalize(worldNormal);
-/*     float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
-    const float biasModifier = 0.5f;
-    if (layer == numCascades)
-        bias *= 1 / (farPlane * biasModifier);
-    else
-        bias *= 1 / (cascadePlaneDistances[layer/4][layer%4] * biasModifier);
- */
+    vec4 fragPosLightSpace = lightSpaceMatrices[layer] * vec4(worldPos, 1.0);
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    projCoords = projCoords * 0.5 + 0.5;
+    float lsDepth = projCoords.z;
+    if (lsDepth > 1.0)
+        return 0.0;
+    float shadowAmount = ShadowPCF(projCoords.xy, layer, lsDepth);
+    if (layer == numCascades - 1)
+        return shadowAmount;
 
-    float bias = 0.0005;
- 
-    // PCF
-    float shadow = 0.0;
-    float shadowDepth = texture(shadowMap, vec3(projCoords.xy, layer)).r;
-    shadow += (currentDepth - bias) > shadowDepth ? 0.7 : 0.0;
-    vec2 texelSize = 1.0 / vec2(textureSize(shadowMap, 0));
-    for (int x = -1; x <= 1; ++x) {
-        for (int y = -1; y <= 1; ++y) {
-            float pcfDepth = texture(shadowMap, vec3(projCoords.xy + vec2(x, y) * texelSize, layer)).r;
-            shadow += (currentDepth - bias) > pcfDepth ? shadowMagnitude : 0.0;
-        }    
-    }
-    shadow /= 9.0;
-        
-    return shadow;
+    // Blend with next cascade if possible:
+    float vsFrustumLength = layer == 0 ?
+        vsNextPlaneDistance :
+        vsNextPlaneDistance - cascadePlaneDistances[(layer-1)/4][(layer-1)%4];
+
+    float distToEdge = (vsNextPlaneDistance - vsDepth) / vsFrustumLength;
+    float blendStart = 0.9;
+    if (1 - distToEdge < blendStart)
+        return shadowAmount;
+
+    // 1. check if worldPos falls into next cascade
+    vec4 lsPosNextCascade = lightSpaceMatrices[layer+1] * vec4(worldPos, 1.0);
+    vec3 tsPosNextCascade = lsPosNextCascade.xyz / lsPosNextCascade.w;
+    tsPosNextCascade = tsPosNextCascade * 0.5 + 0.5;
+    float maxCoord = max(max(abs(tsPosNextCascade.x), abs(tsPosNextCascade.y)), abs(tsPosNextCascade.z));
+    if (maxCoord > 1)
+        return shadowAmount;
+    // 2. if it does, calculate the shadow value in that next cascade
+    float shadowAmountNextCascade = ShadowPCF(tsPosNextCascade.xy, layer+1, tsPosNextCascade.z);
+    // 3. blend with shadowAmount, according to distToEdge
+    float mixAmount = smoothstep(0.0f, 1 - blendStart, distToEdge);
+    float totalShadow = mix(shadowAmountNextCascade, shadowAmount, mixAmount);
+    return totalShadow;
 }
